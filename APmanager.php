aa803c94449399d388d7473ec82005b9cb9d2d48
Alyssa DiRe <dire.alyssa@gmail.com>
2017-11-03 15:45:16 -0400

    Add files via upload

---
+++ APmanager.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Manager Home Page</title>
+</head>
+
+<body>
+
+<div id = "container">
+
+		<?php
+			session_start();
+			if(!isset($_SESSION['uname'])) 
+				header('LOCATION: APlogin.php');
+			else {
+			    $uname = $_SESSION['uname'];
+			    $fname = $_SESSION['fname'];
+			}
+			
+		?>
+		<h1>Welcome, <?php echo   $fname ?></h1>
+		
+		<p>
+		SI Auto Parts Sales System is run out of Staten Island, New York.</p>
+
+		
+	</div>
+
+	
+	
+	</div>
+</body>
+</html>
